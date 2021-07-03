b4a140afb0cdb3cd98d699e8536f141979697976
Glitch (anpi-kk-13) <none>
2021-07-03 12:05:43 +0000

    Rewound to commit 1643c135e033185c88e0b391639dabfb51d6695e + 0 edits

--- server.js
+++ server.js
@@ -1,19 +1,52 @@
-var express = require('express');
-var app = express();
+const path = require("path");
 
-// we've started you off with Express, 
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
-
-// http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
-
-// http://expressjs.com/en/starter/basic-routing.html
-app.get('/', function(request, response) {
-  response.sendFile(__dirname + '/src/pages/index.hbs');
+// Require the fastify framework and instantiate it
+const fastify = require("fastify")({
+  // set this to true for detailed logging:
+  logger: false
 });
 
-// listen for requests :)
-var listener = app.listen(process.env.PORT, function() {
-  console.log('Hello World');
-  console.log('Your app is listening on port ' + listener.address().port);
+// Setup our static files
+fastify.register(require("fastify-static"), {
+  root: path.join(__dirname, "public"),
+  prefix: "/" // optional: default '/'
+});
+
+// fastify-formbody lets us parse incoming forms
+fastify.register(require("fastify-formbody"));
+
+// point-of-view is a templating manager for fastify
+fastify.register(require("point-of-view"), {
+  engine: {
+    handlebars: require("handlebars")
+  }
+});
+
+// Our main GET home page route, pulls from src/pages/index.hbs
+fastify.get("/", function(request, reply) {
+  // params is an object we'll pass to our handlebars template
+  let params = {
+    greeting: "Hello Node!"
+  };
+  // request.query.paramName <-- a querystring example
+  reply.view("/src/pages/index.hbs", params);
+});
+
+// A POST route to handle form submissions
+fastify.post("/", function(request, reply) {
+  let params = {
+    greeting: "Hello Form!"
+  };
+  // request.body.paramName <-- a form post example
+  reply.view("/src/pages/index.hbs", params);
+});
+
+// Run the server and report out to the logs
+fastify.listen(process.env.PORT, function(err, address) {
+  if (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+  console.log(`Your app is listening on ${address}`);
+  fastify.log.info(`server listening on ${address}`);
 });
